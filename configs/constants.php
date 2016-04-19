69848b5797ca2a8d52f930c96ca1783646f1f7e3
Pavel Golman <pavel.golman@postindustria.com>
2016-04-19 22:23:04 +0300

    init backend styles

--- configs/constants.php
+++ configs/constants.php
@@ -2,3 +2,5 @@
 
 const CATEGORIES_TYPE_ID = 3;
 const APP_PATH = '/var/www/html/domtkani.dp.ua/';
+const UPLOAD_PATH = '/var/www/html/domtkani.dp.ua/uploads/';
+const IMAGES_PATH = '/public/images/';
