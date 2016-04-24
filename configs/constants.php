2f8c0a7dfebdfcbe54449eb70ad1ca7c61ce124f
Pavel Golman <pavel.golman@postindustria.com>
2016-04-24 20:46:29 +0300

    fix

--- configs/constants.php
+++ configs/constants.php
@@ -4,5 +4,5 @@ const CATEGORIES_TYPE_ID = 3;
 const APP_PATH = '/var/www/html/dom.tkani.dp/';
 const UPLOAD_PATH = '/var/www/html/dom.tkani.dp/public/uploads/';
 const UNSORTED_IMAGES_PATH = '/var/www/html/dom.tkani.dp/public/uploads/unsorted/';
-const PUBLIC_UPLOADS_PATH = '/public/uploads/unsorted/';
+const PUBLIC_UPLOADS_PATH = '/uploads/unsorted/';
 const IMAGES_PATH = '/public/images/';
