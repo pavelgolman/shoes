6f075c56bc8f2c305494cdf0715fad85fb743c09
Pavel Golman <pavel.golman@postindustria.com>
2016-04-26 19:51:39 +0300

    fix

--- models/Shoes.php
+++ models/Shoes.php
@@ -19,9 +19,9 @@ class Shoes extends Model
 
     public function initialize()
     {
-        $this->hasMany("id", "\Models\ShoesImages", "shoes_id",  array(
-            'alias' => 'shoesImages'
-        ));
+        //$this->hasMany("id", "\Models\ShoesImages", "shoes_id",  array(
+        //    'alias' => 'shoesImages'
+        //));
         $this->hasOne('main_image_id', "\Models\ShoesImages", 'shoes_id', array(
             'alias' => 'image'
         ));
