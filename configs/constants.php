7059ec70e1785c0f456e44b22ea5743bb19767ac
Pavel Golman <pavel.golman@postindustria.com>
2016-04-26 07:32:08 +0300

    fix

--- configs/constants.php
+++ configs/constants.php
@@ -4,5 +4,22 @@ const CATEGORIES_TYPE_ID = 3;
 const APP_PATH = '/var/www/html/dom.tkani.dp/';
 const UPLOAD_PATH = '/var/www/html/dom.tkani.dp/public/uploads/';
 const UNSORTED_IMAGES_PATH = '/var/www/html/dom.tkani.dp/public/uploads/unsorted/';
+const THUMBNAILS_IMAGES_PATH = '/var/www/html/dom.tkani.dp/public/uploads/thumbnails/';
 const PUBLIC_UPLOADS_PATH = '/uploads/unsorted/';
-const IMAGES_PATH = '/public/images/';
+const PUBLIC_THUMBNAILS_PATH = '/uploads/thumbnails/';
+
+
+$SHOES_THUMBNAILS_MAP = array(
+    array(
+        'width' => 125,
+        'height' => 125
+    ),
+    array(
+        'width' => 250,
+        'height' => 250
+    ),
+    array(
+        'width' => 85,
+        'height' => 85
+    )
+);
--- models/ShoesImages.php
+++ models/ShoesImages.php
@@ -33,11 +33,23 @@ class ShoesImages extends Model
     }
 
     public function thumbnailURL($width, $height){
-        //return APP_PATH."public/images/shoes/".$this->shoes_id.'/'.$this->storage_id.'/'.$width.'_'.$height.'.'.$this->extension;
-        return PUBLIC_UPLOADS_PATH.$this->upload_date_year.'/'.$this->upload_date_month.'/'.$this->upload_date_day.'/'.$this->storage_id . '.' . $this->extension;
+        return PUBLIC_THUMBNAILS_PATH.$this->shoes_id.'/'.$this->storage_id.'/'.$width.'_'.$height.'.'.$this->extension;
     }
 
     public function generateUniqueHash(){
         $this->storage_id = Phalcon\Text::random(Phalcon\Text::RANDOM_ALNUM, 8);
+    }
+
+    public function reGenerateThumbnails(){
+        global $SHOES_THUMBNAILS_MAP;
+        foreach($SHOES_THUMBNAILS_MAP as $size){
+            $image = new Phalcon\Image\Adapter\Imagick($this->originalPath());
+            $image->resize($size['width'], $size['height']);
+            var_dump(APP_PATH.'public'.$this->thumbnailURL($size['width'], $size['height']));
+            if ($image->save(APP_PATH.'public'.$this->thumbnailURL($size['width'], $size['height']))) {
+
+            }
+        }
+        die();
     }
 }
