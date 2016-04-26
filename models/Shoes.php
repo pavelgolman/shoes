f4d60c96c0b9223aaa3fbb3238fbaaca7046ef20
Pavel Golman <pavel.golman@postindustria.com>
2016-04-26 19:46:07 +0300

    fix

--- models/Shoes.php
+++ models/Shoes.php
@@ -15,6 +15,8 @@ class Shoes extends Model
 
     public $price;
 
+    public $main_image_id;
+
     public function initialize()
     {
         $this->hasMany("id", "\Models\ShoesImages", "shoes_id",  array(
