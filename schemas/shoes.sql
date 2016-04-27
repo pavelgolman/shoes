d77ad8fabfa50e1fe2a3e4b672ccd53d62fbe4f8
Pavel Golman <pavel.golman@postindustria.com>
2016-04-27 05:21:20 +0300

    fix

--- schemas/shoes.sql
+++ schemas/shoes.sql
@@ -137,7 +137,7 @@ create table if not exists promo_blocks_shoes(
   shoes_id int unsigned not null,
   promo_blocks_id int unsigned not null,
    key unq(shoes_id, promo_blocks_id)
-)engine=InnoDb DEFAULT CHARACTER SET=utf8;
+)engine = InnoDb DEFAULT CHARACTER SET=utf8 COLLATE utf8_unicode_ci;
 
 
 TRUNCATE TABLE promo_blocks_shoes;
