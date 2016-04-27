99ba9751b31dd198f69dff9edc3d6ec71207fa52
Pavel Golman <pavel.golman@postindustria.com>
2016-04-27 05:30:33 +0300

    fix

--- schemas/shoes.sql
+++ schemas/shoes.sql
@@ -11,6 +11,7 @@ create table if not exists shoes(
   index article(article)
 )engine = InnoDb DEFAULT CHARACTER SET=utf8;
 
+update shoes set main_image_id = (select id from shoes_images where shoes_images.shoes_id = shoes.id limit 1);
 
 drop table if exists categories;
 create table if not exists categories(
