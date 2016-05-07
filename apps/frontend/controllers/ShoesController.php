c834d817151fe5b500cc893aee536c788b82ae7b
Pavel Golman <pavel.golman@postindustria.com>
2016-05-07 18:29:06 +0300

    fix

--- apps/frontend/controllers/ShoesController.php
+++ apps/frontend/controllers/ShoesController.php
@@ -3,13 +3,26 @@
 namespace Multiple\Frontend\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Paginator\Adapter\Model as PaginatorModel;
 
 class ShoesController extends Controller
 {
 
     public function indexAction()
     {
+        $currentPage = 1;
 
+        $shoes      = \Models\Shoes::find();
+
+        $paginator   = new PaginatorModel(
+            array(
+                "data"  => $shoes,
+                "limit" => 100,
+                "page"  => $currentPage
+            )
+        );
+
+        $this->view->shoes = $paginator->getPaginate();
     }
 
 }
