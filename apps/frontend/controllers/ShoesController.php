4a57398128adc0c002e7ffed251627e77d434a5b
Pavel Golman <pavel.golman@postindustria.com>
2016-05-07 18:17:34 +0300

    fix

---
+++ apps/frontend/controllers/ShoesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Multiple\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ShoesController extends Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
