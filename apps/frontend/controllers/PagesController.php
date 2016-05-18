a87fda8710321fe07d05979afdb4b6350a17ce87
Pavel Golman <pavel.golman@postindustria.com>
2016-05-19 00:10:08 +0300

    fix

---
+++ apps/frontend/controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Multiple\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class PagesController extends Controller
+{
+
+    public function contactAction()
+    {
+
+    }
+
+}
