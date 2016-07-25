916998c27472f658997d963f03b16925315c20d9
Pavel Golman <pavel.golman@postindustria.com>
2016-07-26 00:01:22 +0300

    fix

--- apps/frontend/controllers/SitemapController.php
+++ apps/frontend/controllers/SitemapController.php
@@ -67,10 +67,10 @@ XML;
                 }
             }
             $xml .= '<categoryId>'.$category_id.'</categoryId>'."\n";
-            //$xml .= '<priceuah>'.$s->price.'</priceuah>'."\n";
-            //$xml .= '<image>http://rumi.store'.$s->mainImage->originalURL().'</image>'."\n";
+            $xml .= '<price>'.$s->price.'</price>'."\n";
+            $xml .= '<image>http://rumi.store'.$s->mainImage->originalURL().'</image>'."\n";
             $xml .= '<vendorCode>'.$s->article.'</vendorCode>'."\n";
-            $xml .= '<description><![CDATA['.$s->description->description.']]</description>'."\n";
+            $xml .= '<description><![CDATA['.$s->description->description.']]></description>'."\n";
 
             $xml .= '<available>true</available>'."\n";
 
