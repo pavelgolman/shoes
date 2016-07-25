1f675afb7c1b2b35ac4edebbcefa62b7efe8ca03
Pavel Golman <pavel.golman@postindustria.com>
2016-07-25 23:03:08 +0300

    fix

--- apps/frontend/controllers/SitemapController.php
+++ apps/frontend/controllers/SitemapController.php
@@ -50,7 +50,7 @@ XML;
                 $s->artile,
                 $s->name,
                 'Купить обувь Днепр наличие заказ',
-                $s->description,
+                $s->description->description,
                 'u',
                 $s->price,
                 'UAH',
