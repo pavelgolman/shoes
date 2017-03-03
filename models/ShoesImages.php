f210a288c3dcbdf76ec94c5ba8dd0708655c699c
Pavel Golman <pavel.golman@postindustria.com>
2017-03-03 20:30:55 +0200

    fix

--- models/ShoesImages.php
+++ models/ShoesImages.php
@@ -67,7 +67,7 @@ class ShoesImages extends Model
         $image = new Phalcon\Image\Adapter\Imagick($this->originalPath());
         $image->resize($width, $height);
 
-        $path = APP_PATH . 'public' . $this->thumbnailURL($width, $height, true, false);
+        $path = APP_PATH . 'public' . $this->thumbnailURL($width, $height, false, false);
         if (is_dir($path) == false) {
             mkdir($path, 0777, true);
         }
